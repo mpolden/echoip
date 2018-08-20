6d915d0f962a8ed222da428d079b3cb56676389c
rufo <rufus@rufus.al>
2018-08-20 03:03:49 +0100

    use big int for decimal IP rather than uint64

--- iputil/iputil_test.go
+++ iputil/iputil_test.go
@@ -1,21 +1,26 @@
 package iputil
 
 import (
+	"math/big"
 	"net"
 	"testing"
 )
 
 func TestToDecimal(t *testing.T) {
+	var msb = new(big.Int)
+	msb, _ = msb.SetString("80000000000000000000000000000000", 16)
+
 	var tests = []struct {
 		in  string
-		out uint64
+		out *big.Int
 	}{
-		{"127.0.0.1", 2130706433},
-		{"::1", 1},
+		{"127.0.0.1", big.NewInt(2130706433)},
+		{"::1", big.NewInt(1)},
+		{"8000::", msb},
 	}
 	for _, tt := range tests {
 		i := ToDecimal(net.ParseIP(tt.in))
-		if tt.out != i {
+		if tt.out.Cmp(i) != 0 {
 			t.Errorf("Expected %d, got %d for IP %s", tt.out, i, tt.in)
 		}
 	}
