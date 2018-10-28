0b87064a33e87fb2b1cd667d380cf91f3b4b4a92
johannrichard <johann.richard@mycvs.org>
2018-10-28 12:30:12 +0100

    Lat/Lon Box

--- cmd/echoip/main.go
+++ cmd/echoip/main.go
@@ -48,7 +48,7 @@ func main() {
 		log.Printf("Trusting header(s) %+v to contain correct remote IP", opts.IPHeaders)
 	}
 
-	log.Printf("Listening on http://%s", opts.Listen)
+	log.Printf("Listening on http://%s", os.Getenv("PORT") || opts.Listen)
 	if err := server.ListenAndServe(opts.Listen); err != nil {
 		log.Fatal(err)
 	}
