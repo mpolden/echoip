2b7e1de81fe599c7109f2c86169b61bb293ea4a9
Christian Korneck <christian@korneck.de>
2019-01-13 14:47:54 +0100

    Update http.go
    fix error message

--- http/http.go
+++ http/http.go
@@ -114,7 +114,7 @@ func (s *Server) newPortResponse(r *http.Request) (PortResponse, error) {
 	lastElement := filepath.Base(r.URL.Path)
 	port, err := strconv.ParseUint(lastElement, 10, 16)
 	if err != nil || port < 1 || port > 65535 {
-		return PortResponse{Port: port}, fmt.Errorf("invalid port: %d", port)
+		return PortResponse{Port: port}, fmt.Errorf("invalid port: %s", lastElement)
 	}
 	ip, err := ipFromRequest(s.IPHeaders, r)
 	if err != nil {
